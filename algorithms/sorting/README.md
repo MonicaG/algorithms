fa79562f4a528ffba6f5f6199a03a9c19ddc63b3
Monica Granbois <mgranbois@outlook.com>
2021-12-10 09:11:53 -0800

    O(n^2) sort algorithms

---
+++ algorithms/sorting/README.md
@@ -0,0 +1,14 @@
+# Sorting Algorithms
+
+Note: videos are not mine. They are just resources I found helpful.
+## O(N<sup>2</sup>)
+
+### Bubble Sort 
+[![Bubble Sort](https://img.youtube.com/vi/xli_FI7CuzA/0.jpg)](https://youtu.be/xli_FI7CuzA)
+
+### Selection Sort
+[![Insertion Sort](https://img.youtube.com/vi/g-PGLbMth_g/0.jpg)](https://youtu.be/g-PGLbMth_g)
+
+### Insertion Sort 
+[![Insertion Sort](https://img.youtube.com/vi/JU767SDMDvA/0.jpg)](https://youtu.be/JU767SDMDvA)
+
---
+++ algorithms/sorting/sort_algorithms.py
@@ -0,0 +1,41 @@
+# Short circuit BubbleSort implementation. If no elements are swapped then we know the list is sorted.
+# In this case we do not need to do anymore passes of the array and can exit the algorithm
+def bubble_sort(nums: [int]):
+    did_swap_elements = True
+    # subtract one here so we don't have to worry about index out of bounds error when using the right_item_index below
+    end_range = len(nums) - 1
+    while did_swap_elements:
+        did_swap_elements = False
+        for left_item_index in range(end_range):
+            right_item_index = left_item_index + 1
+            if nums[left_item_index] > nums[right_item_index]:
+                nums[left_item_index], nums[right_item_index] = nums[right_item_index], nums[left_item_index]
+                did_swap_elements = True
+        # end_range is decremented because we know the tail end is sorted, so no need to operate on it again.
+        end_range -= 1
+    return nums
+
+
+def selection_sort(nums: [int]):
+    # 1 is subtracted from the len(nums) because by the last element, the array will be sorted. So don't need to
+    # process it again.
+    for start_index in range(len(nums) - 1):
+        low_index = start_index
+        # 1 is added to the start_index, because there is no point in comparing the first start_index to itself.
+        for i in range(start_index + 1, len(nums)):
+            if nums[i] < nums[low_index]:
+                low_index = i
+        if start_index != low_index:
+            nums[start_index], nums[low_index] = nums[low_index], nums[start_index]
+    return nums
+
+
+def insertion_sort(nums: [int]):
+    for index in range(1, len(nums)):
+        temp = nums[index]
+        check_index = index - 1
+        while check_index >= 0 and temp < nums[check_index]:
+            nums[check_index + 1] = nums[check_index]
+            check_index -= 1
+        nums[check_index + 1] = temp
+    return nums
