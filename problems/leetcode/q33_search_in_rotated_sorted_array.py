f9a85aa198ae39a7bf122aa80d7472bc70569ba0
Monica Granbois <mgranbois@outlook.com>
2022-01-23 21:11:35 -0800

    Commented code

--- problems/leetcode/q33_search_in_rotated_sorted_array.py
+++ problems/leetcode/q33_search_in_rotated_sorted_array.py
@@ -21,26 +21,37 @@ class Solution:
         return recurse(0, len(nums) - 1)
 
     def search(self, nums: List[int], target: int) -> int:
+        # [7, 8, 9, 10, 0, 1, 2, 3, 4, 5, 6]
         left = 0
         right = len(nums) - 1
         while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
+            mid = (right + left) // 2
+            if target == nums[mid]:
                 return mid
-            if nums[mid] < nums[right]:
-                # we are in an ordered part of the array:
-                if nums[mid] <= target <= nums[right]:
+            elif nums[mid] > nums[right]:
+                # in the left side of the array
+                if target > nums[mid] or target < nums[left]:
+                    # Example: [5, 6, 7, 8, 9, 0, 1, 2, 3, 4]
+                    # Assume mid point is index 2 (value 7). We need to search to the right of the mid point for the
+                    # cases: 1) the target is greater than the mid (i.e. target is 9) or 2) the target is less than
+                    # the mid point, and the value is actually on the right side of the array (i.e. target is 3)
                     left = mid + 1
                 else:
+                    # This scenario is reached when we just need to search the left side of the array. This occurs when
+                    # the target value is less than or equal to the mid point and the target is on the left side of the
+                    # array (i.e. target is 6)
                     right = mid - 1
             else:
-                # we are in an un ordered part of the array
-                if nums[mid] >= target >= nums[left]:
+                # in the right side of array
+                if target < nums[mid] or target > nums[right]:
+                    # Example: [5, 6, 7, 8, 9, 0, 1, 2, 3, 4]
+                    # Assume mid point is index 8 (value 3). We need to search to the left of the mid point for the
+                    # cases: 1) target is less than the mid (i.e. target is 0) or 2) the target is greater than the mid
+                    # point, but the value is actually on the left side of the array (i.e. target is 6)
                     right = mid - 1
                 else:
+                    # This scenario is reached when we need to search just the right side of the array. This occurs when
+                    # the target value is greater or equal to the mid point value and the target is on the right side of
+                    # the array (i.e. target 4).
                     left = mid + 1
         return -1
-
-
-
-
